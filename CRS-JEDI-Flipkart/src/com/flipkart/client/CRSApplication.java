e0b35caf3c7f6d166f1425c75f022198bf61a5e5
Ashish Patel <ashishpatel.1992@gmail.com>
2021-01-21 16:53:56 +0530

    added gitignore

--- CRS-JEDI-Flipkart/src/com/flipkart/client/CRSApplication.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/client/CRSApplication.java
@@ -17,55 +17,61 @@ public class CRSApplication {
         Scanner scanner = new Scanner(System.in);
         int choice;
 
-        while (true) {
-            logger.info("=== Login MENU ===");
-            logger.info("Login As:-");
-            logger.info("1. Student");
-            logger.info("2. Professor");
-            logger.info("3. Admin");
-            logger.info("4. Exit");
+       try{
+           while (true) {
+               logger.info("=== Login MENU ===");
+               logger.info("Login As:-");
+               logger.info("1. Student");
+               logger.info("2. Professor");
+               logger.info("3. Admin");
+               logger.info("4. Exit");
 
-            choice = scanner.nextInt();
-            String userId;
-            String userPassword;
+               choice = scanner.nextInt();
+               String userId;
+               String userPassword;
 
-            logger.info("Enter User ID ? ");
-            userId = scanner.next();
-            logger.info("Enter User Password ? ");
-            userPassword = scanner.next();
+               logger.info("Enter User ID ? ");
+               userId = scanner.next();
+               logger.info("Enter User Password ? ");
+               userPassword = scanner.next();
 
-            UserInterface userInterface = new UserOperation();
-            StudentCRSClient studentCRSClient = new StudentCRSClient(userId);
+               UserInterface userInterface = new UserOperation();
+               StudentCRSClient studentCRSClient = new StudentCRSClient(userId);
 
-            if (loginStatus || userInterface.verifyCredentials(userId, userPassword)) {
-                logger.info("User Authenticated");
-                loginStatus = true;
-                switch (choice) {
-                    case 1:
-                        logger.info("Student is selected");
-                        studentCRSClient.studentMenu();
-                        break;
-                    case 2:
-                        logger.info("Professor is selected");
-                        break;
-                    case 3:
-                        logger.info("Admin is selected");
-                        break;
-                    case 4:
-                        logger.info("Exit is selected");
-                        System.exit(0);
-                        return;
+               if (loginStatus || userInterface.verifyCredentials(userId, userPassword)) {
+                   logger.info("User Authenticated");
+                   loginStatus = true;
+                   switch (choice) {
+                       case 1:
+                           logger.info("Student is selected");
+                           studentCRSClient.studentMenu();
+                           break;
+                       case 2:
+                           logger.info("Professor is selected");
+                           break;
+                       case 3:
+                           logger.info("Admin is selected");
+                           break;
+                       case 4:
+                           logger.info("Exit is selected");
+                           System.exit(0);
+                           return;
 
-                    default:
-                        logger.info("Student is selected");
+                       default:
+                           logger.info("Student is selected");
 
 
-                }
-            } else {
-                logger.info("Invalid userId or password");
-            }
+                   }
+               } else {
+                   logger.info("Invalid userId or password");
+               }
 
-        }
+           }
+       }catch(Exception e){
+           logger.error(e.getMessage());
+       }finally {
+           scanner.close();
+       }
 
 
     }
