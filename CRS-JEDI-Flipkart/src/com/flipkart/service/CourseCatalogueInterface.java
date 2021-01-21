099e902d513ada61aee805597474195808c57959
Ashish Patel <ashishpatel.1992@gmail.com>
2021-01-21 11:35:18 +0530

    Added comments for CourseCatalogueInterface and UserInterface

--- CRS-JEDI-Flipkart/src/com/flipkart/service/CourseCatalogueInterface.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/service/CourseCatalogueInterface.java
@@ -5,9 +5,38 @@ import com.flipkart.bean.Course;
 import java.util.ArrayList;
 
 public interface CourseCatalogueInterface {
+    /**
+     * returns the list of all courses
+     * @return
+     */
     public ArrayList<Course> getCourseList();
+
+    /**
+     * creates a new course
+     * @param courseId
+     * @param courseName
+     * @return
+     */
     public boolean addCourse(String courseId, String courseName);
+
+    /**
+     * Returns the course along with its details
+     * @param courseId
+     * @return
+     */
     public Course getCourse(String courseId);
+
+    /**
+     * Delete a specific course
+     * @param courseId
+     * @return
+     */
     public boolean deleteCourse(String courseId);
+
+    /**
+     * Assign a professor to a course
+     * @param courseId
+     * @return
+     */
     public boolean assignProfessor(String courseId);
 }
--- CRS-JEDI-Flipkart/src/com/flipkart/service/UserInterface.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/service/UserInterface.java
@@ -1,6 +1,12 @@
 package com.flipkart.service;
 
 public interface UserInterface {
+    /**
+     * Verify credentials of the student
+     * @param id
+     * @param hasedPassword
+     * @return
+     */
     public boolean verifyCredentials(String id, String hasedPassword);
 
 }
