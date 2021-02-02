357fc2b6c418b5657f105eb88a948cfb728397ec
Ashish Patel <ashishpatel.1992@gmail.com>
2021-02-02 09:49:40 +0530

    dropwizard service
    Added comments and pom dependency

--- CRS-JEDI-dropwizard/src/main/java/com/flipkart/dropwizard/Service.java
+++ CRS-JEDI-dropwizard/src/main/java/com/flipkart/dropwizard/Service.java
@@ -8,8 +8,15 @@ import io.dropwizard.Configuration;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
+/**
+ * Service class handles initialization of REST API Classes for DropWizard
+ */
 public class Service extends Application<Configuration> {
+
     public static void main(String[] args) throws Exception {
+        /**
+         * Creates service and runs it
+         */
         new Service().run(args);
     }
 
@@ -17,6 +24,13 @@ public class Service extends Application<Configuration> {
     public void initialize(Bootstrap<Configuration> b) {
     }
 
+    /**
+     * Provides configuration files and environment variables as parameters.
+     * This function is used to register REST API classes
+     * @param configuration
+     * @param environment
+     * @throws Exception
+     */
     @Override
     public void run(Configuration configuration, Environment environment) throws Exception {
         environment.jersey().register(new AdminRESTAPI());
